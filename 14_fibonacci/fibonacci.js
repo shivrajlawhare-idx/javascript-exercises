23dd639a3c1d19ef81309d15a56df1abcfca9dc3
shivraj lawhare <shivrajlawhare@indexnine.com>
2025-09-03 13:06:14 +0530

    14

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if(n<0){
+        return "OOPS";
+    }else if(n==0){
+        return 0;
+    }else if(n==1 || n==2){
+        return 1;
+    }
+    let arr = [1,1];
+    let temp = 0;
+    for(let i=2;i<n;i++){
+        temp = arr[i-1] + arr[i-2];
+        arr.push(temp);
+    }
+    return arr[n-1];
 };
 
 // Do not edit below this line
