8ec5a4b117d03e00a97f2df666005dabce3315f0
shivraj lawhare <shivrajlawhare@indexnine.com>
2025-09-03 11:51:40 +0530

    08

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr,...x) {
+    let ans = [];
+    arr.forEach(element => { 
+        if(!x.includes(element)){
+            ans.push(element);
+        }
+    });
+    return ans;
 };
 
 // Do not edit below this line
