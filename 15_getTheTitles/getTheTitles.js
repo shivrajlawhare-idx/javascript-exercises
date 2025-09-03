127e80cc5903153a83212029c36ca4e3752bb62b
shivraj lawhare <shivrajlawhare@indexnine.com>
2025-09-03 13:11:43 +0530

    15

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
+const { e } = require("../04_mathEquations/mathEquations");
 
+const getTheTitles = function(arr) {
+    let ans = [];
+    arr.forEach(e => {
+        ans.push(e.title);
+    });
+    return ans;
 };
 
 // Do not edit below this line
