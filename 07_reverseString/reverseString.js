f0a061e72e7fc0aece136a00bcc4717b50a012af
shivraj lawhare <shivrajlawhare@indexnine.com>
2025-09-03 11:43:51 +0530

    07

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let ans = "";
+    let len = word.length;
+    for(let i=len-1;i>=0;i--){
+        ans+=word[i];
+    }
+    return ans;
 };
 
 // Do not edit below this line
