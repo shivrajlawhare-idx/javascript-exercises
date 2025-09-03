d891e3c673e6af1e3d0dbce7bb2d524a6f55c290
shivraj lawhare <shivrajlawhare@indexnine.com>
2025-09-03 12:51:48 +0530

    12

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(x,y) {
+	return x+y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x,y) {
+	return x-y;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let ans =0;
+	arr.forEach(e => {
+    ans+=e;
+  }); 
+  return ans;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let ans =1;
+	arr.forEach(e => {
+    ans*=e;
+  }); 
+  return ans;
 };
 
-const power = function() {
-	
+const power = function(x,y) {
+	return x**y;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	if(n == 0){
+    return 1;
+  }
+  let ans = 1;
+  for(let i=1;i<=n;i++){
+    ans*=i;
+  }
+  return ans;
 };
 
 // Do not edit below this line
