e4c2c4536c4808a9077c3fe698a09905af1e84ae
shivraj lawhare <shivrajlawhare@indexnine.com>
2025-09-03 14:48:29 +0530

    16

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    return people.reduce((oldest, person) => {
+        const currentYear = new Date().getFullYear();
+        const age = (person.yearOfDeath || currentYear) - person.yearOfBirth;
+        const oldestAge = (oldest.yearOfDeath || currentYear) - oldest.yearOfBirth;
 
+        return age > oldestAge ? person : oldest;
+    });
 };
 
 // Do not edit below this line
