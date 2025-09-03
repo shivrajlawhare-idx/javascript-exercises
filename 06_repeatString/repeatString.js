3fe1aee5510ac34b12d5e9a3362080f1845592d3
shivraj lawhare <shivrajlawhare@indexnine.com>
2025-09-03 11:34:52 +0530

    06

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(word, n) {
+    if (n < 0) return "ERROR";
+    let ans = "";
+    for(let i=1;i<=n;i++){
+        ans += word;
+    }
+    return ans;
 };
 
 // Do not edit below this line
