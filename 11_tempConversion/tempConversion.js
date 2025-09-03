ecaea4557cffda8236630fbd6240c4cb4bd41141
shivraj lawhare <shivrajlawhare@indexnine.com>
2025-09-03 12:38:10 +0530

    11

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  let ans = (f-32)*(5/9);
+  ans = parseFloat(ans.toFixed(1));
+  return ans;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  let ans = (c*(9/5) + 32);
+  ans = parseFloat(ans.toFixed(1));
+  return ans;
 };
 
 // Do not edit below this line
